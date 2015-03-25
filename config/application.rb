7a3a71e47177e6c60ceeec4e9de1cfd838d9244d
Vladimir Yartsev <vovayartsev@gmail.com>
2015-03-25 23:29:33 +0400

    RSpec added

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,17 @@ Bundler.require(*Rails.groups)
 
 module CloudVault
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixture: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      # g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
