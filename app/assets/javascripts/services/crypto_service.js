0de45ba5d7f26bd636891c99902fbcc761bc083c
Vladimir Yartsev <vovayartsev@gmail.com>
2015-04-30 15:06:51 +0400

    Refactored crypto_service. KeyManager introduced

--- app/assets/javascripts/services/crypto_service.js
+++ app/assets/javascripts/services/crypto_service.js
@@ -1,22 +1,29 @@
-angular.module('vault').service('CryptoService', function ($rootScope, $q, $timeout, P3SKB, MetaService, KeybaseLoginService) {
-    var me = null,
-      sharedKeyManager = null,
-      cachedRequestSignature = null;
+angular.module('vault').service('CryptoService', function ($rootScope, $q, $timeout, P3SKB, MetaService, KeyManagerService, KeybaseLoginService) {
+    var me = null, requestSignature = null;
 
     this.login = function (credentials) {
       progress(20)();
       return KeybaseLoginService.login(credentials.kbLogin, credentials.kbPassword)
-        .then(saveMe).then(progress(50))
-        .then(injectPublicKey).then(loadKey).then(savePublicKey).then(progress(75))
-        .then(injectPrivateKey(credentials.kbPassword)).then(mergePgpPrivate).then(progress(100))
-        .then(buildRequestSignature).then(saveRequestSignature)
+        .then(saveMe)
+        .then(progress(50))
+
+        .then(injectPublicKey)
+        .then(KeyManagerService.loadPublicKey)
+        .then(progress(75))
+
+        .then(injectPrivateKey(credentials.kbPassword))
+        .then(KeyManagerService.mergePgpPrivate)
+        .then(progress(100))
+
+        .then(injectCsrfToken)
+        .then(KeyManagerService.buildRequestSignature)
+        .then(saveRequestSignature)
     }
 
-    // TODO: remove hardcoded username and request signature
     this.requestHeaders = function () {
       return {
         'X-Kb-Login': kbLogin(),
-        'X-Kb-Signature': requestSignature()
+        'X-Kb-Signature': requestSignature
       }
     }
 
@@ -41,10 +48,7 @@ angular.module('vault').service('CryptoService', function ($rootScope, $q, $time
       return me;
     }
 
-    function savePublicKey(key) {
-      sharedKeyManager = key;
-    }
-
+    // TODO: never used?
     kbLogin = function () {
       return me && me.basics.username;
     }
@@ -53,48 +57,19 @@ angular.module('vault').service('CryptoService', function ($rootScope, $q, $time
       return me.public_keys.primary.bundle;
     }
 
-    function loadKey(inKey) {
-      return $q(function (resolve, reject) {
-        kbpgp.KeyManager.import_from_armored_pgp({armored: inKey}, function (err, keyManager) {
-          err ? reject("loadKey: " + err) : resolve(keyManager);
-        })
-      })
-    }
-
-
-    function requestSignature() {
-      // it's lifetime is 10 minutes, should be re-generated often
-      return cachedRequestSignature;
-    }
-
-    // TODO - implement
-    // this is  CSRF_TOKEN -> PGP -> BASE64
-    function buildRequestSignature() {
-      var params = {msg: MetaService.csrfToken(), sign_with: sharedKeyManager};
-      return $q(function (resolve, reject) {
-        kbpgp.box(params, function (err, result_string, result_buffer) {
-          err ? reject(err) : resolve(Base64.encode(result_string))
-        })
-      })
-    }
-
-    function saveRequestSignature(sig) {
-      cachedRequestSignature = sig;
-    }
-
     function injectPrivateKey(passphrase) {
       return function () {
         return P3SKB.armor(me.private_keys.primary.bundle, passphrase);
       }
     }
 
-    function mergePgpPrivate(privateKey) {
-      return $q(function (resolve, reject) {
-        sharedKeyManager.merge_pgp_private({armored: privateKey}, function (err) {
-          err ? reject(err) : resolve();
-        });
-      });
+    function injectCsrfToken() {
+      return MetaService.csrfToken();
     }
 
+    function saveRequestSignature(sig) {
+      requestSignature = sig;
+    }
   }
-);
+)
+;
---
+++ app/assets/javascripts/services/key_manager_service.js
@@ -0,0 +1,32 @@
+angular.module('vault').service('KeyManagerService', function ($q) {
+    var sharedKeyManager = null;
+
+    this.loadPublicKey = function (inKey) {
+      return $q(function (resolve, reject) {
+        kbpgp.KeyManager.import_from_armored_pgp({armored: inKey}, function (err, keyManager) {
+          err ? reject("loadKey: " + err) : resolve(keyManager);
+        })
+      }).then(function (mgr) {
+        sharedKeyManager = mgr;
+      })
+    }
+
+    this.mergePgpPrivate = function (privateKey) {
+      return $q(function (resolve, reject) {
+        sharedKeyManager.merge_pgp_private({armored: privateKey}, function (err) {
+          err ? reject(err) : resolve();
+        });
+      });
+    }
+
+    // this is  CSRF_TOKEN -> PGP -> BASE64
+    this.buildRequestSignature = function (csrfToken) {
+      var params = {msg: csrfToken, sign_with: sharedKeyManager};
+      return $q(function (resolve, reject) {
+        kbpgp.box(params, function (err, result_string, result_buffer) {
+          err ? reject(err) : resolve(Base64.encode(result_string))
+        })
+      })
+    }
+  }
+);
