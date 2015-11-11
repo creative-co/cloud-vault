a3365f753156053827fd21233b45d664a4f2fcc3
Krutov Alex <alex.krutov@castle.co>
2015-11-11 20:41:07 +0400

    WIP: Users list

--- app/assets/javascripts/directives/user_autocomplete.js
+++ app/assets/javascripts/directives/user_autocomplete.js
@@ -1,45 +1,59 @@
 angular.module('vault').directive('userAutocomplete', function (KeybaseUserAutocompleteService, $timeout) {
   return {
-    require: 'ngModel',
     restrict: 'E',
-    replace: true,
-    template: '<input type=text placeholder="Username, e-mail, github..."/>',
-    link: function(scope, element, attr, ngModel) {
-      // Initialize
-      var PAUSE = 500;
-      scope.completions = [];
-      scope.searchTimer = null;
+    template: '<span>' +
+      '<input type=text ng-model="search" placeholder="Username, e-mail, github..."/>' +
+      '<ul>' +
+        '<li ng-repeat="item in completions">' +
+          '<img ng-src="{{item.thumbnail}}" />' +
+          '{{item.components.username.val}}' +
+        '</li>' +
+      '</ul>' +
+    '</span>',
+    compile: function compile(tElement, tAttrs) {
+      tElement.find('input').addClass(tAttrs.class);
+      tElement.removeClass();
 
-      // Fetch completions on change
-      scope.$watch(function () {
-        return ngModel.$modelValue;
-      }, function(query) {
-        if (scope.searchTimer) {
-          $timeout.cancel(scope.searchTimer);
-        }
-
-        if (_.isEmpty(query)) {
-          return false;
-        }
-
-        scope.searchTimer = $timeout(function() {
-          KeybaseUserAutocompleteService.search(query)
-              .then(updateCompletions);
-        }, PAUSE);
-      });
-
-      function updateCompletions(completions) {
-        scope.completions = completions;
-        console.log(completions);
-      }
-
-      // Remove timers when directive destroyed
-      scope.$on(
-          '$destroy',
-          function() {
-            $timeout.cancel(scope.searchTimer);
-          }
-      );
+      return postLink;
     }
   };
+
+  function postLink(scope, element, attr, ngModel) {
+    // Initialize
+    var PAUSE = 500;
+    scope.search = '';
+    scope.completions = [];
+    scope.searchTimer = null;
+    scope.activeState = true;
+
+    // Fetch completions on change
+    scope.$watch(function () {
+      return scope.search;
+    }, function(query) {
+      if (scope.searchTimer) {
+        $timeout.cancel(scope.searchTimer);
+      }
+
+      if (_.isEmpty(query)) {
+        return false;
+      }
+
+      scope.searchTimer = $timeout(function() {
+        KeybaseUserAutocompleteService.search(query)
+            .then(updateCompletions);
+      }, PAUSE);
+    });
+
+    function updateCompletions(completions) {
+      scope.completions = completions;
+    }
+
+    // Remove timers when directive destroyed
+    scope.$on(
+        '$destroy',
+        function() {
+          $timeout.cancel(scope.searchTimer);
+        }
+    );
+  }
 });
